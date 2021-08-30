9a2dab56f1458304572efa3a21cae1c12bedf1c9
Joel Alanko <joel@DESKTOP-TORPGTD.localdomain>
2021-08-30 13:37:23 +0300

    Saving ppm img to file

--- weekendpy/main.py
+++ weekendpy/main.py
@@ -16,3 +16,40 @@ def cmdline_args():
 if __name__ == '__main__':
 	args = cmdline_args()
 	print(args)
+
+	# Image
+	width = args.width
+	height = args.height
+
+	# Render
+	imgFile = open("image.ppm", "w")
+
+	imgFile.write("P3\n{} {}\n255\n".format(width, height))
+
+	for j in range(height, 0, -1):
+		for i in range(width):
+			r = i / (width - 1)
+			g = j / (height - 1)
+			b = 0.25
+
+			ir = int(255.999 * r)
+			ig = int(255.999 * g)
+			ib = int(255.999 * b)
+
+			imgFile.write("{} {} {}\n".format(ir, ig, ib))
+
+	imgFile.close()
+
+#	for (int j = height-1; j >= 0; --j) {
+#		for (int i = 0; i < image_width; ++i) {
+#			auto r = double(i) / (image_width-1);
+#			auto g = double(j) / (image_height-1);
+#			auto b = 0.25;
+#
+#			int ir = static_cast<int>(255.999 * r);
+#			int ig = static_cast<int>(255.999 * g);
+#			int ib = static_cast<int>(255.999 * b);
+#
+#			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+#		}
+#	}
