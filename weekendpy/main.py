7d74621948ad78943c54ed85b73e705bb080cf34
Joel Alanko <joel@DESKTOP-TORPGTD.localdomain>
2021-08-30 13:23:30 +0300

    Initialized python version of rt in wkend

---
+++ weekendpy/main.py
@@ -0,0 +1,18 @@
+import sys
+import argparse
+
+def cmdline_args():
+	p = argparse.ArgumentParser(add_help=False)
+
+
+	p.add_argument("-w", "--width", type=int, default=520,
+					help="output screen width")
+
+	p.add_argument("-h", "--height", type=int, default=520,
+					help="output screen height")
+
+	return(p.parse_args())
+
+if __name__ == '__main__':
+	args = cmdline_args()
+	print(args)
